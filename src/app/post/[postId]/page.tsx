decf4cce05a2048cf07b01391c94561a31b7ae9d
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2025-02-02 16:42:13 +0700

    fetch api

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -3,6 +3,10 @@ type PostBlogs = {
     postId: string;
   };
 };
+export type Reactions = {
+  likes: number;
+  dislikes: number;
+};
 
 export default async function getPost({ params }: PostBlogs) {
   const response = await fetch(`https://dummyjson.com/post/${params.postId}`);
@@ -10,13 +14,21 @@ export default async function getPost({ params }: PostBlogs) {
     return <div>not found</div>;
   }
 
-  const { id, title, body } = await response.json();
+  const {
+    id,
+    title,
+    body,
+    reactions,
+  }: { id: number; title: string; body: string; reactions: Reactions } =
+    await response.json();
 
   return (
     <div>
       <h1>
         post {id} : {title}
       </h1>
+
+      <p> {reactions.likes}</p>
     </div>
   );
 }
